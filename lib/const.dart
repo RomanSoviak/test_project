477c58b05c5d300838c3136f102bfe67ea386983
Prystash <prystash.igor@gmail.com>
2021-10-25 12:21:29 +0300

    Test commit

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+class Constamts {
+
+static const String kAppBartitle = "Головна";
+static const String KTitle = "Привіт";
+
+
+}
---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+    
+    final kDarktheme = ThemeData.dark().copyWith(
+      appBarTheme: const AppBarTheme(
+        color: Colors.black,
+        foregroundColor: Colors.white,
+      )
+    ) ;
+
+
+     final kLightTheme = ThemeData.dark().copyWith(
+    appBarTheme: const AppBarTheme(
+      foregroundColor: Colors.black87,
+      backgroundColor: Colors.white,
+    )
+) ;
